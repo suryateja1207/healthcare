2be0ab3c456911e718ec5f03d7de08ef304caf95
suryateja1207 <suryateja.aerra@gmail.com>
2025-06-20 15:19:33 +0530

    Update healthify.py

--- healthify.py
+++ healthify.py
@@ -1,30 +1,7 @@
 import subprocess
 import sys
 import os
-import requests
 
-API_URL = "https://router.huggingface.co/sambanova/v1/chat/completions"
-headers = {
-    
-}
-
-def query(payload):
-    response = requests.post(API_URL, headers=headers, json=payload)
-    return response.json()
-
-response = query({
-    "messages": [
-        {
-            "role": "user",
-            "content": "What is the capital of France?"
-        }
-    ],
-    "model": "Meta-Llama-3.1-8B-Instruct"
-})
-
-print(response["choices"][0]["message"])
-
-#####################################################
 def install_package(package):
     """Install a package using pip"""
     try:
